b0fe58e83b72f3952da4d52bc35c6a0d22d3ab38
mdaslam81 <mdaslam81@gmail.com>
2020-11-10 10:45:56 -0800

    commit

--- SQLQuery_2.sql
+++ SQLQuery_2.sql
@@ -1,1 +1,15 @@
-Select top 10 * from jobs 
+CREATE VIEW [dbo].[ViewName]
+AS
+/**********************************************************************************************************************
+Creation Date :Date  Created By : Name 
+Purpose : 
+Used By: SAIT
+Database :
+Updates : 
+Date                       Author                       purpose
+------------              ------------------           ---------------------
+***********************************************************************************************************************/
+BEGIN 
+SET NOCOUNT ON
+SET TRANSACTION ISOLATION LEVEL READ UNCOMMITTED
+END 
